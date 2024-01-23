ffb3c44780be94f53312a07a5594be276cfb9ff8
Finnedorn <daniele.giuntoli@outlook.com>
2024-01-23 18:22:07 +0100

    added itemdetails page with axios call

---
+++ src/assets/pages/ItemDetail.vue
@@ -0,0 +1,43 @@
+<template>
+    <div v-if="Item">
+        <h1>{{ item.name }}</h1>
+        <div class="w-25">
+            <img :src="store.imagePath+item.img"  :alt="item.name">
+        </div>
+        <h6>{{ item.category }}</h6>
+        <h6>{{ item.type }}</h6>
+        <h6>{{ item.weight }}</h6>
+        <h6>{{ item.cost }}</h6>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+import { store } from "@/data/store";
+
+export default {
+    name: 'ItemDetails',
+    data() {
+        return {
+            store,
+            item: []
+        }
+    },
+    methods: {
+        getItemDetail() {
+            axios.get(this.store.apiUrl+"/items"+this.$route.params.slug).then((res)=>{
+                if(res.data.results) {
+                    this.item = res.data.results
+                }else{
+                    this.$route.push({name: "not-found"})
+                }
+            })
+        },
+    },
+    mounted() {
+        this.getItemDetail()
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
