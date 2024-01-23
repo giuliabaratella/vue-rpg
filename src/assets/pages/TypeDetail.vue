1e20f678e7ca0590bda81f3c5442ad0b644052b1
chiara1911 <chiaracesari89@gmail.com>
2024-01-23 18:22:35 +0100

    add typeDetail

---
+++ src/assets/pages/TypeDetail.vue
@@ -0,0 +1,42 @@
+<template>
+     <div v-if="type">
+  <h1>Show Type</h1>
+      <h3 >{{type.name}}</h3>
+      <img :src="store.imagePath + type.img" :alt="type.name">
+    </div> 
+  </template>
+  
+  <script>
+  import axios from "axios";
+  import { store } from "../../data/store";
+  export default {
+    name: "TypeDetail",
+  
+    data() {
+      return {
+        store,  
+        type:[]    
+      };
+    },  
+    methods: {
+      getTypeDetail() {
+        console.log('ciao')
+                axios
+          .get(this.store.apiUrl+ "/types/"+ this.$route.params.id)
+          .then((res) => {
+            if (res.data.results) {
+                this.type = res.data.results;
+        //   } else {
+        //     this.$router.push({name: not-found});
+        }                     
+          });
+      },
+    },
+    mounted() {
+      this.getTypeDetail();
+    },
+  };
+  </script>
+  
+  <style lang="scss" scoped></style>
+  
