c9ce37e41cc7d0d4548bc5f18b7c94430064c35e
giuliabaratella <giuliabaratella7@gmail.com>
2024-01-23 15:56:02 +0100

    scaffolding

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <router-view :key="$route.path"></router-view>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/assets/pages/AppHome.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Home</h1>
+</template>
+
+<script>
+    export default {
+        name:'AppHome',
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
