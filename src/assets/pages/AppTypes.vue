5a68abcf0cddde41352affce4696c4ae742e5d11
Riccardo Potalivo <riccardopotalivo@gmail.com>
2024-01-23 16:35:54 +0100

    pages types set

---
+++ src/assets/pages/AppTypes.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+    import { store } from "../../data/store.js";
+
+    export default {
+        name: "AppTypes",
+
+        components: {
+
+        },
+
+        data() {
+            return {
+                store,
+            };
+        },
+
+        methods: {
+            getAllTypes() {
+                axios
+                .get(store.apiUrl + "/types").then((res) => {
+                    console.log(res.data);
+                    this.store.types = res.data.result.data;
+                })
+            },
+        },
+
+        mounted() {
+            this.getAllTypes();
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
