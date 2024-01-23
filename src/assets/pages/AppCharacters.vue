df778321a5e653d948468e92335cd681e1d6b3c0
ericaostini <ericaostini14@gmail.com>
2024-01-23 16:31:53 +0100

    fix v-for

--- src/assets/pages/AppCharacters.vue
+++ src/assets/pages/AppCharacters.vue
@@ -2,7 +2,7 @@
    <div class="container text-center">
       <h1 class="m-3">Characters</h1>
       <div class="row row-cols-5 g-3 justify-content-center">
-         <div v-for="character in store.characters">
+         <div v-for="character in this.store.characters">
             <h2>{{ character.name }}</h2>
          </div>
       </div>
@@ -12,7 +12,6 @@
 <script>
 import { store } from '@/data/store';
 import axios from 'axios';
-
 export default {
    name: 'AppCharacter',
    data() {
@@ -20,7 +19,7 @@ export default {
    },
    methods: {
       allCharacters() {
-         axios.get(`${this.store.apiUrl}/characters`)
+         axios.get(store.apiUrl + `/characters`)
             .then((res) => {
                console.log(res.data.results);
                this.store.characters = res.data.results
