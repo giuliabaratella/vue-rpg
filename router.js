ea4a9786e2085b4596f0e8a564d4c97e57857766
Finnedorn <daniele.giuntoli@outlook.com>
2024-01-23 16:29:47 +0100

    added new routes on router.js, added new NavbarComponent and new Notfound page

--- router.js
+++ router.js
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import AppHome from './src/assets/pages/AppHome.vue'
+import AppHome from './src/assets/pages/AppHome.vue';
+import AppCharacters from './src/assets/pages/AppCharacters.vue';
+import AppTypes from './src/assets/pages/AppTypes.vue';
+import AppItems from './src/assets/pages/AppItems.vue';
+import NotFound from './pages/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,7 +13,26 @@ const router = createRouter({
       name: 'home',
       component: AppHome
     },
-   
+    {
+      path:"/characters",
+      name:"characters",
+      component: AppCharacters
+    },
+    {
+      path:"/types",
+      name:"types",
+      component: AppTypes
+    },
+    {
+      path:"/items",
+      name:"Items",
+      component: AppItems
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      name:"not-found",
+      component: NotFound
+    },
   ]
 })
 
---
+++ src/assets/pages/NotFound.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>Page not found !</h1>
+</template>
+  
+<script>
+export default {
+    name: 'NotFound',
+    components: {
+    },
+    data() {
+        return {
+        };
+    },
+    methods: {
+    },
+    mounted() {
+    }
+}
+</script>
+  
+<style lang="scss" scoped>
+// @use "./assets/style/partials/variables" as *;
+</style>
