1c3181d75ad35ff14def8aeb4c6bf8f5d5ef80be
ericaostini <ericaostini14@gmail.com>
2024-01-24 20:01:57 +0100

    get random computer card and user card with select

--- src/assets/pages/AppGame.vue
+++ src/assets/pages/AppGame.vue
@@ -1,58 +1,74 @@
 <template>
    <div>
       ciao
-      <button @click="generateComputerCard()">Genera</button>
+      <select v-model="characterSelected" @change="CharacterId()">
+         <option v-for="character in this.store.characters" class="m-3" :value="character.id">{{ character.name }}</option>
+      </select>
+      <button @click="generateComputerCharacter()">Genera</button>
       <button @click="startBattle()">Inizia</button>
    </div>
 </template>
  
 <script>
+import axios from 'axios';
+import { store } from '../../data/store';
 export default {
    name: 'App',
    data() {
       return {
-         playerCard:
-         {
-            name: 'Warrior',
-            life: 100,
-            attack: 40,
-            defense: 70
-         },
+         store,
+         playerCard: null,
          computerCard: null,
+         characterSelected: "",
          results: "",
          round: 0
       }
    },
    methods: {
-      startBattle() {
-         this.computerCard = this.generateComputerCard();
-         this.playerCard.life += this.playerCard.defense;
-         this.playerCard.life -= this.computerCard.attack;
-         console.log(this.playerCard);
-         this.computerCard.life += this.computerCard.defense;
-         this.computerCard.life -= this.playerCard.attack;
-         console.log(this.computerCard);
-         this.results = this.compareCards(this.playerCard, this.computerCard);
-         console.log(this.results);
-      },
+      // startBattle() {
+      //    this.computerCard = this.generateComputerCard();
+      //    this.playerCard.life += this.playerCard.defense;
+      //    this.playerCard.life -= this.computerCard.attack;
+      //    console.log(this.playerCard);
+      //    this.computerCard.life += this.computerCard.defense;
+      //    this.computerCard.life -= this.playerCard.attack;
+      //    console.log(this.computerCard);
+      //    this.results = this.compareCards(this.playerCard, this.computerCard);
+      //    console.log(this.results);
+      // },
 
       //genera carta random computer
-      generateComputerCard() {
-         const names = ['Warrior', 'Sorcerer', 'Rogue', 'Paladin'];
-         const randomName = names[Math.floor(Math.random() * names.length)];
-         console.log(randomName);
-         const randomAttack = Math.floor(Math.random() * 100);
-         console.log(randomAttack);
-         const randomDefense = Math.floor(Math.random() * 100);
-         console.log(randomDefense);
-         const randomLife = Math.floor(Math.random() * 100);
-         console.log(randomLife);
-         return {
-            name: randomName,
-            life: randomLife,
-            attack: randomAttack,
-            defense: randomDefense,
-         }
+      generateComputerCharacter() {
+         axios.get(store.apiUrl + '/characters')
+            .then((res) => {
+               const lastCharacterIndex = res.data.results.length - 1;
+               const randomIndex = Math.floor(Math.random() * lastCharacterIndex);
+               console.log(randomIndex);
+               console.log(res.data.results.length);
+               console.log(res.data.results);
+               this.computerCard = res.data.results[randomIndex];
+               console.log(this.computerCard);
+               console.log(this.computerCard.life);
+               // this.computerCard.life -= 10;
+               // console.log(this.computerCard);
+            })
+      },
+      allCharacters() {
+         axios.get(store.apiUrl + '/characters')
+            .then((res) => {
+               console.log(res.data.results);
+               this.store.characters = res.data.results
+            })
+      },
+      CharacterId() {
+         // console.log(this.characterSelected);
+         this.store.characters.forEach(character => {
+            // console.log(character);
+            if (character.id == this.characterSelected) {
+               this.playerCard = character;
+            }
+         });
+         console.log(this.playerCard)
       },
       compareCards(playerCard, computerCard) {
          if (playerCard.life > computerCard.life) {
@@ -63,6 +79,10 @@ export default {
             return "Pareggio";
          }
       }
+   },
+   mounted() {
+      this.allCharacters();
+      this.generateComputerCharacter();
    }
 }
 </script>
