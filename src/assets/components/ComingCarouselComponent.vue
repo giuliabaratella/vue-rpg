7ce1fc5ff3cccb66b01de4b78840fa0a298f22a3
Finnedorn <daniele.giuntoli@outlook.com>
2024-01-26 17:42:47 +0100

    added new carousel component fixed some router link issues

---
+++ src/assets/components/ComingCarouselComponent.vue
@@ -0,0 +1,118 @@
+<template>
+    <div class="continaer-fluid coming-bg">
+        <div class="container">
+            <Carousel  :itemsToShow="1.50" :wrapAround="true" :transition="500">
+                <Slide v-for="slide in slideImgs" :key="slide">
+                    <div class="carousel__item">
+                        <img :src="slide.img" :alt="slide.title" />
+                    </div>
+                </Slide>
+                <template #addons>
+                    <Pagination/>
+                    <Navigation>
+                        <template #next id="right">
+                            <img src="../images/arrow-right.png" alt="arraw-right">
+                        </template>
+                        <template #prev>
+                            <img src="../images/arrow-left.png" alt="arraw-left">
+                        </template>
+                    </Navigation>
+                </template>
+            </Carousel>
+        </div>
+    </div>
+</template>
+  
+<script>
+import { defineComponent } from 'vue'
+import { Carousel, Pagination, Slide, Navigation } from 'vue3-carousel'
+
+import 'vue3-carousel/dist/carousel.css'
+
+export default defineComponent({
+    name: 'ComingCarouselComponent',
+    components: {
+        Carousel,
+        Slide,
+        Pagination,
+        Navigation
+    },
+    data() {
+        return {
+            slideImgs: [
+                {
+                    img: '../../../public/images/carousel_comingsoon_1.jpg',
+                    title: 'Coming-Soon-01',
+                },
+                {
+                    img: '../../../public/images/carousel_comingsoon_2.jpg',
+                    title: 'Coming-Soon-02',
+                },
+                {
+                    img: '../../../public/images/carousel_comingsoon_3.jpg',
+                    title: 'Coming-Soon-03',
+                },
+                {
+                    img: '../../../public/images/carousel_comingsoon_4.jpg',
+                    title: 'Coming-Soon-04',
+                }
+                
+            ]
+        }
+    },
+})
+</script>
+  
+<style lang="scss" scoped>
+.carousel__slide {
+    padding: 5px;
+}
+
+.carousel__viewport {
+    perspective: 2000px;
+}
+
+.carousel__track {
+    transform-style: preserve-3d;
+}
+
+.carousel__slide--sliding {
+    transition: 0.5s;
+}
+
+.carousel__slide {
+    opacity: 0.9;
+    transform: rotateY(-20deg) scale(0.9);
+}
+
+.carousel__slide--active~.carousel__slide {
+    transform: rotateY(13.5deg) scale(0.95);
+}
+
+.carousel__slide--prev {
+    opacity: 1;
+    filter: brightness(60%);
+    transform: rotateY(-10deg) scale(0.95);
+}
+
+.carousel__slide--next {
+    opacity: 1;
+    filter: brightness(60%);
+    transform: rotateY(10deg) scale(0.85);
+}
+
+.carousel__slide--active {
+    opacity: 1;
+    transform: rotateY(0) scale(1.10);
+}
+
+
+.coming-bg {
+    background-image: url(../images/bg-coming-soon.jpg);
+    background-size: contain;
+}
+
+
+
+</style>
+  
--- src/assets/pages/AppHome.vue
+++ src/assets/pages/AppHome.vue
@@ -2,18 +2,21 @@
     <HeroComponent/>
     <AboutComponent/>
     <AdvBanner/>
+    <ComingCarouselComponent/>
 </template>
 
 <script>
 import AdvBanner from '../components/AdvBanner.vue';
 import HeroComponent from '../components/HeroComponent.vue';
 import AboutComponent from '../components/AboutComponent.vue';
+import ComingCarouselComponent from '../components/ComingCarouselComponent.vue';
     export default {
         name:'AppHome',
         components:{
             AdvBanner,
             HeroComponent,
             AboutComponent,
+            ComingCarouselComponent
         }
     }
 </script>
