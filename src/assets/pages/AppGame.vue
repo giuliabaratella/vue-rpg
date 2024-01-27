dd89dde684cf82bf98eb41f9f4c467b05154868d
ericaostini <ericaostini14@gmail.com>
2024-01-27 12:16:21 +0100

    display game result

--- src/assets/pages/AppGame.vue
+++ src/assets/pages/AppGame.vue
@@ -4,9 +4,9 @@
       <h1>Play</h1>
       <div class="row">
 
-         <div class="col-4"  id="playerCard">
+         <div class="col-4" id="playerCard">
 
-            <div class="flip-card-inner" >
+            <div class="flip-card-inner">
 
                <div class="flip-card-front" :class="{ 'overflow-y-scroll': !playerData }">
                   <div class="m-3" v-if="!playerData">
@@ -63,7 +63,8 @@
                         <h5>speed: {{ characterSelected.speed }}</h5>
 
                         <div class="d-flex" id="selectItems">
-                           <div @click="selectItem(item, characterSelected.attack, index)" v-for="(item, index) in characterSelected.items">
+                           <div @click="selectItem(item, characterSelected.attack, index)"
+                              v-for="(item, index) in characterSelected.items">
                               <img class="w-50 " :src="store.imagePath + item.img" :alt="item.name">
                               <span>{{ item.attack }}</span>
                            </div>
@@ -80,6 +81,7 @@
 
             <div v-if="game" class="mb-3">
                <h1>Game: {{ game }}</h1>
+               <h3>Computer: {{ countWinComputer }} - Player: {{ countWinPlayer }}</h3>
             </div>
 
             <h2 id="startPlayer" class="mb-3"></h2>
@@ -165,10 +167,12 @@ export default {
          onGoingBattle: false,
          round: 0,
          game: 0,
+         countWinComputer: 0,
+         countWinPlayer: 0
       }
    },
    methods: {
-      
+
       allCharacters() {
          axios.get(store.apiUrl + '/characters')
             .then((res) => {
@@ -189,20 +193,20 @@ export default {
             setTimeout(() => {
                this.playerData = this.characterSelected;
             }, 200)
-            
-   
+
+
             const lastCharacterIndex = store.characters.length - 1;
             const randomIndex = Math.floor(Math.random() * lastCharacterIndex);
             const rndCharacter = store.characters[randomIndex];
 
             rndCharacter.items[0].attack >= rndCharacter.items[1].attack ?
-            this.computerItem = rndCharacter.items[0] :
-            this.computerItem = rndCharacter.items[1];
+               this.computerItem = rndCharacter.items[0] :
+               this.computerItem = rndCharacter.items[1];
 
             this.computerAttack = this.computerItem.attack + rndCharacter.attack;
-            
+
             this.computerData = rndCharacter;
-   
+
             document.querySelectorAll('.flip-card-inner')[0].classList.add('rotate_card');
             document.querySelectorAll('.flip-card-inner')[1].classList.add('rotate_card');
 
@@ -340,10 +344,12 @@ export default {
 
          if (player.life <= 0) {
             this.results = 'computer win'
-            return console.log('computer win');
+            this.countWinComputer++;
+            console.log('computer win');
          } else if (computer.life <= 0) {
             this.results = 'player win';
-            return console.log('player win');
+            console.log('player win');
+            this.countWinPlayer++;
          }
       },
 
@@ -370,8 +376,8 @@ export default {
          setTimeout(() => {
             this.playerData = '';
             this.computerData = '';
-         }, 200)         
-         
+         }, 200)
+
          this.playerAttack = '';
          this.results = '';
          this.game = 0;
@@ -383,7 +389,7 @@ export default {
    mounted() {
       this.allCharacters();
       // this.generateComputerCharacter();
-      
+
    }
 }
 </script>
@@ -477,19 +483,19 @@ export default {
 // flip card
 
 .flip-card {
-  background-color: transparent;
-  width: 300px;
-  height: 300px;
-  perspective: 1000px;
+   background-color: transparent;
+   width: 300px;
+   height: 300px;
+   perspective: 1000px;
 }
 
 .flip-card-inner {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  transition: transform 0.5s;
-  transform-style: preserve-3d;
-//   box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+   position: relative;
+   width: 100%;
+   height: 100%;
+   transition: transform 0.5s;
+   transform-style: preserve-3d;
+   //   box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
 }
 
 // .flip-card:hover .flip-card-inner {
@@ -497,25 +503,26 @@ export default {
 // }
 
 .rotate_card {
-  transform: rotateY(180deg);
+   transform: rotateY(180deg);
 }
 
-.flip-card-front, .flip-card-back {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  -webkit-backface-visibility: hidden;
-  backface-visibility: hidden;
+.flip-card-front,
+.flip-card-back {
+   position: absolute;
+   width: 100%;
+   height: 100%;
+   -webkit-backface-visibility: hidden;
+   backface-visibility: hidden;
 }
 
 .flip-card-front {
-//   background-color: #bbb;
-//   color: black;
+   //   background-color: #bbb;
+   //   color: black;
 }
 
 .flip-card-back {
-//   background-color: #2980b9;
-//   color: white;
-  transform: rotateY(180deg);
+   //   background-color: #2980b9;
+   //   color: white;
+   transform: rotateY(180deg);
 }
 </style>
